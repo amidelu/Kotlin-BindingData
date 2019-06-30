a7780271a4b49a97e542b384c6e100cc46d0c33e
amidelu <delowar_gtc@yahoo.com>
2019-07-01 00:49:29 +0600

    observable editText using data binding

--- app/src/main/java/com/andro4everyone/bindingdata/Contact.kt
+++ app/src/main/java/com/andro4everyone/bindingdata/Contact.kt
@@ -1,3 +1,22 @@
 package com.andro4everyone.bindingdata
 
-class Contact (var name: String, var email: String)
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class Contact (_name: String, _email: String): BaseObservable() { //extends BaseObservables for observing data changes
+    //getter and setter code for our contact
+    @get: Bindable
+    var name: String = _name
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.name)
+        }
+
+    @get: Bindable
+    var email: String = _email
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.email)
+        }
+
+}
--- app/src/main/java/com/andro4everyone/bindingdata/EventHandler.kt
+++ app/src/main/java/com/andro4everyone/bindingdata/EventHandler.kt
@@ -6,7 +6,7 @@ import android.widget.Toast
 open class EventHandler(context: Context) {
     val myContext: Context = context
 
-    fun btnClicked() {
-        Toast.makeText(myContext, "Horraayy, I am Clicked!", Toast.LENGTH_LONG).show()
+    fun btnClicked(name: String) {
+        Toast.makeText(myContext, "Hello $name", Toast.LENGTH_LONG).show()
     }
 }
