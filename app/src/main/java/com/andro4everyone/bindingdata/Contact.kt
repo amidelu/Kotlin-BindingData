722d9743bc27fd9b7761ea80fcc58a9497b8b6ba
amidelu <delowar_gtc@yahoo.com>
2019-07-01 00:08:37 +0600

    binding data with layout

---
+++ app/src/main/java/com/andro4everyone/bindingdata/Contact.kt
@@ -0,0 +1,3 @@
+package com.andro4everyone.bindingdata
+
+class Contact (var name: String, var email: String)
---
+++ app/src/main/java/com/andro4everyone/bindingdata/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.andro4everyone.bindingdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.andro4everyone.bindingdata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.contact = Contact("Delowar Hossain", "zindadelu@gmail.com")
+    }
+}
